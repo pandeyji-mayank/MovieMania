a8431ca73f8aaa5b661bb3ade7d1000d87359f7e
Mayank Pandey <ssmanku2910@gmail.com>
2024-08-16 16:49:55 +0530

    Infinite Scrolling

--- frontend/src/components/Genre.jsx
+++ frontend/src/components/Genre.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Header from './Header'
 import axios from 'axios'
 import { options } from '../utils/constant'
@@ -10,7 +10,6 @@ const Genre = () => {
     const [genre, setGenre] = useState();
     const [genreId, setGenreId] = useState(28);
     const [movie, setMovie] = useState(null);
-    const [currgenre, setCurrGenre] = useState('Action');
     useEffect(() => {
         const getdetails = async () => {
             try {
@@ -23,12 +22,19 @@ const Genre = () => {
         }
         getdetails();
     }, [])
+
+
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [index, setIndex] = useState(2);
+    const loaderRef = useRef(null);
+
     useEffect(() => {
         const getmoviebyGenre = async () => {
             try {
                 const res = await axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`, options);
                 // console.log(res.data.results);
-                setMovie(res.data.results);
+                items(res.data.results);
 
             } catch (error) {
                 console.log(error);
@@ -37,11 +43,50 @@ const Genre = () => {
         getmoviebyGenre();
     }, [genreId])
 
+
+
+
+    const fetchData = useCallback(async () => {
+        if (isLoading) return;
+        setIsLoading(true);
+        console.log(index);
+        axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${index}&sort_by=popularity.desc&with_genres=${genreId}`, options).then((res) => {
+            setItems((prev) => [...prev, ...res.data.results]);
+
+            console.log(res.data);
+            if (res.data.total_pages > index)
+                setIndex((prev => prev + 1));
+        }).catch((err) => console.log(err));
+        setIsLoading(false);
+    }, [index, isLoading]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const target = entries[0];
+            if (target.isIntersecting) fetchData();
+
+        })
+
+        if (loaderRef.current) {
+            observer.observe(loaderRef.current);
+        }
+        return () => {
+            if (loaderRef.current) {
+                observer.unobserve(loaderRef.current);
+            }
+        }
+    }, [fetchData]);
+
+
+
     const [tempgenresdetails, setTempGenresDetails] = useState({ id: 28, name: 'Action' });
     useEffect(() => {
-        toast.success(`${tempgenresdetails.name + ' Movie'}`);
+        // toast.success(`${tempgenresdetails.name + ' Movie'}`);
         console.log(tempgenresdetails);
         setGenreId(tempgenresdetails.id);
+        setItems([]);
+        setIndex(1);
+        // fetchData();
     }, [tempgenresdetails])
 
 
@@ -49,7 +94,7 @@ const Genre = () => {
         <div>
             <Header />
             <div className='flex flex-row ' >
-                <div className='h-screen w-1/6   flex flex-col '>
+                <div className='sticky left-0 h-screen w-1/6  flex flex-col '>
                     <div className='mt-32'></div>
                     {
                         genre && genre.map((item, ind) => (
@@ -71,17 +116,20 @@ const Genre = () => {
                                 All {tempgenresdetails.name} Movies
                             </h2>
                         </div>
-                        <div className="grid grid-cols-4 gap-4 m-8 ">
-                            {
-                                movie && (
-                                    movie.map((movieItem) => {
-                                        return <MovieTile key={movieItem.id} movieId={movieItem.id} />
-                                    })
-                                )
-                            }
-                        </div>
+                        <div>
 
-                        <div className='h-20  w-full flex justify-center ' ><span className="loading loading-ring loading-lg"></span></div>
+                            <div className="grid grid-cols-4 gap-4 m-8 ">
+                                {
+                                    items && (
+                                        items.map((movieItem) => {
+                                            return <MovieTile key={movieItem.id} movieId={movieItem.id} />
+                                        })
+                                    )
+                                }
+                            </div>
+                            <div ref={loaderRef}>{isLoading && <div className='w-full h-36 flex  justify-center items-center'> <span className="loading loading-ring loading-lg"></span></div>}</div>
+                        </div>
+                        <div className='w-full h-48 flex  justify-center items-center '> <span className="h-56 loading loading-ring loading-lg"></span></div>
                     </div>
                 </div>
             </div>
